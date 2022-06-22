e030eb873bf1d850ffbdf23fe08ebbb6bd6efb76
Geonosis <shishkin.vlad92@yandex.ru>
2022-06-22 21:36:41 +0300

    first commit

---
+++ simpleapp/views.py
@@ -0,0 +1,40 @@
+# Импортируем класс, который говорит нам о том,
+# что в этом представлении мы будем выводить список объектов из БД
+from django.views.generic import ListView, DetailView
+from .models import News
+from datetime import datetime
+
+
+class NewsList(ListView):
+    # Указываем модель, объекты которой мы будем выводить
+    model = News
+    # Указываем имя шаблона, в котором будут все инструкции о том,
+    # как именно пользователю должны быть показаны наши объекты
+    template_name = 'simpleapp/news.html'
+    # Это имя списка, в котором будут лежать все объекты.
+    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
+    context_object_name = 'news'
+    queryset = News.objects.order_by('-dateCreation')
+
+    # Метод get_context_data позволяет нам изменить набор данных,
+    # который будет передан в шаблон.
+    def get_context_data(self, **kwargs):
+        # С помощью super() мы обращаемся к родительским классам
+        # и вызываем у них метод get_context_data с теми же аргументами,
+        # что и были переданы нам.
+        # В ответе мы должны получить словарь.
+        context = super().get_context_data(**kwargs)
+        # К словарю добавим текущую дату в ключ 'time_now'.
+        context['time_now'] = datetime.utcnow()
+        # Добавим ещё одну пустую переменную,
+        # чтобы на её примере рассмотреть работу ещё одного фильтра.
+        context['next_upgrade'] = "Обновление каждый четверг"
+        return context
+
+class NewsDetail(DetailView):
+    # Модель всё та же, но мы хотим получать информацию по отдельному товару
+    model = News
+    # Используем другой шаблон — product.html
+    template_name = 'simpleapp/news_detail.html'
+    # Название объекта, в котором будет выбранный пользователем продукт
+    context_object_name = 'news_detail'
